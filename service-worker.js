c80cafd4cb2593fe84ea726527179e48b19b3f52
afunk <alexfunk1222@gmail.com>
2020-06-08 14:02:16 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.cccea2ae0fbe977c4bf1d7ec1664287b.js"
+  "/portfolio/precache-manifest.66ac42ac0cad1118121b1d42f8ca63b4.js"
 );
 
 self.addEventListener('message', (event) => {
